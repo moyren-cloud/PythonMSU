e7b25be22605c280517b28c5ff5d783ff3bd80a2
Maria <mgercke@yandex.ru>
2025-04-14 19:02:29 +0300

    Update and rename Лекция5задание2 to Лекция5задание2.py

--- Лекция5задание2.py
+++ Лекция5задание2.py
@@ -7,5 +7,5 @@
 
 my_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
 new_list = [my_list[i] for i in range(1, len(my_list)) if my_list[i] > my_list[i - 1]]
-print(f"Исходный список: {my_list}")
-print(f"Новый список: {new_list}")
+print(f'Исходный список: {my_list}')
+print(f'Новый список: {new_list}')
